e69ab3d804276a050eed6030abf4fcc626096c20
rfyiamcool <rfyiamcool@163.com>
2019-06-13 20:03:41 +0800

    add spinlock

---
+++ spinlock.go
@@ -0,0 +1,35 @@
+package spinlock
+
+import (
+	"runtime"
+	"sync/atomic"
+)
+
+type SpinLock struct {
+	f uint32
+}
+
+func New() *SpinLock {
+	return &SpinLock{}
+}
+
+func (sl *SpinLock) Lock() {
+	for !sl.TryLock() {
+		runtime.Gosched() //allow other goroutines to do stuff.
+	}
+}
+
+func (sl *SpinLock) Unlock() {
+	atomic.StoreUint32(&sl.f, 0)
+}
+
+func (sl *SpinLock) TryLock() bool {
+	return atomic.CompareAndSwapUint32(&sl.f, 0, 1)
+}
+
+func (sl *SpinLock) String() string {
+	if atomic.LoadUint32(&sl.f) == 1 {
+		return "Locked"
+	}
+	return "Unlocked"
+}
